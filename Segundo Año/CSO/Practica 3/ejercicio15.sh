2b0f941feb9f3cf393bfe8daf01b34aa10c6a641
LunaCuervo99 <98750266+LunaCuervo99@users.noreply.github.com>
2022-10-07 19:11:54 -0300

    Create ejercicio15.sh

---
+++ Segundo Año/CSO/Practica 3/ejercicio15.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Comando cut. El comando cut nos permite procesar la líneas de la entrada que reciba
+#(archivo, entrada estándar, resultado de otro comando, etc) y cortar columnas o campos,
+#siendo posible indicar cual es el delimitador de las mismas. Investigue los parámetros que
+#puede recibir este comando y cite ejemplos de uso.
