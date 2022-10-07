53866c13968371b71951dd1a2d31ea8b05d0d4d7
LunaCuervo99 <98750266+LunaCuervo99@users.noreply.github.com>
2022-10-07 19:14:27 -0300

    Update ejercicio15.sh

--- Segundo Año/CSO/Practica 3/ejercicio15.sh
+++ Segundo Año/CSO/Practica 3/ejercicio15.sh
@@ -4,3 +4,4 @@
 #(archivo, entrada estándar, resultado de otro comando, etc) y cortar columnas o campos,
 #siendo posible indicar cual es el delimitador de las mismas. Investigue los parámetros que
 #puede recibir este comando y cite ejemplos de uso.
+
