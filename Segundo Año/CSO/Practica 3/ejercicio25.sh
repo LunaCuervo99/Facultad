dc80b176d8d2c9c9e4cd0a789e1b35105cd99cee
LunaCuervo99 <98750266+LunaCuervo99@users.noreply.github.com>
2022-10-10 10:42:00 -0300

    Create ejercicio25.sh

---
+++ Segundo Año/CSO/Practica 3/ejercicio25.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Realice un script que agregue en un arreglo todos los nombres de los usuarios del sistema
+#pertenecientes al grupo “users”. Adicionalmente el script puede recibir como parametro:
+#“-b n”: Retorna el elemento de la posición n del arreglo si el mismo existe. Caso
+#contrario, un mensaje de error.
+#“-l”: Devuelve la longitud del arreglo
+#“-i”: Imprime todos los elementos del arreglo en pantalla
