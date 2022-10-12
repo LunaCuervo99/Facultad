840905b95bb87dd5780aa0c6cf1bef4c5234b39d
LunaCuervo99 <98750266+LunaCuervo99@users.noreply.github.com>
2022-10-12 15:53:07 -0300

    Create ejercicio27.sh

---
+++ Segundo Año/CSO/Practica 3/ejercicio27.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+#Realice un script que implemente a través de la utilización de funciones las operaciones
+#básicas sobre arreglos:
+#inicializar: Crea un arreglo llamado array vacío
+#agregar_elem <parametro1>: Agrega al final del arreglo el parámetro recibido
+#eliminar_elem <parametro1>: Elimina del arreglo el elemento que se encuentra en la
+#posición recibida como
