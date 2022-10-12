fbbcfa9ed45f2f4a426cd5fd40b28479e204f699
LunaCuervo99 <98750266+LunaCuervo99@users.noreply.github.com>
2022-10-12 15:21:37 -0300

    Update ejercicio25.sh

--- Segundo Año/CSO/Practica 3/ejercicio25.sh
+++ Segundo Año/CSO/Practica 3/ejercicio25.sh
@@ -6,3 +6,40 @@
 #contrario, un mensaje de error.
 #“-l”: Devuelve la longitud del arreglo
 #“-i”: Imprime todos los elementos del arreglo en pantalla
+
+if ( $# -lt 1 ); 
+then
+  echo "No se pasaron los parametros correctos"
+  exit 1
+else
+  usuarios=$(cat /etc/group | grep "users:" | cut -d ":" -f 4)
+  j=0
+  vector=()
+  for i in $usuarios
+  do
+    vector[j]=$i
+    let j++
+  done
+  case $1 in 
+  do
+    "-b")
+      if [ $2 -gt $(expr ${#vector[*]} -1) ];
+      then
+        echo "La posicion se va de rango"
+      else
+        echo "El elemento en la posicion $2 es: ${vector[$2]}"
+      fi
+    ;;
+    "-l")
+      echo "Longitud: ${#vector[*]}"
+    ;;
+    "-i")
+      echo "Elementos del vector: "
+      for i in  ${vector[*]}
+      do
+        echo "$i"
+      done 
+    ;;
+  esac
+fi
+exit 0
