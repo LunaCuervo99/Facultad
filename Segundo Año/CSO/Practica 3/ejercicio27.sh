53d53279a6c10397151a1bae13e40d61b972b22c
LunaCuervo99 <98750266+LunaCuervo99@users.noreply.github.com>
2022-10-12 15:54:23 -0300

    Update ejercicio27.sh

--- Segundo Año/CSO/Practica 3/ejercicio27.sh
+++ Segundo Año/CSO/Practica 3/ejercicio27.sh
@@ -6,3 +6,4 @@
 #agregar_elem <parametro1>: Agrega al final del arreglo el parámetro recibido
 #eliminar_elem <parametro1>: Elimina del arreglo el elemento que se encuentra en la
 #posición recibida como
+
