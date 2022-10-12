77728a2bb1bcc31605408962be559ea6a7d45192
LunaCuervo99 <98750266+LunaCuervo99@users.noreply.github.com>
2022-10-12 16:31:33 -0300

    Update ejercicio28.sh

--- Segundo Año/CSO/Practica 3/ejercicio28.sh
+++ Segundo Año/CSO/Practica 3/ejercicio28.sh
@@ -7,3 +7,27 @@
 #dichos valores en pantalla. En caso de encontrar subdirectorios, no deberán procesarse, y
 #tampoco deberán ser tenidos en cuenta para la suma a informar.
 
+if [ $# -lt 1 ];
+then
+  echo "No se enviaron parametros"
+  exit 1
+else
+  if [ -e $1 ]
+  then
+    cant=0
+    for i in $(ls -l $1)
+    do
+      if ( -f $1/$i )
+      then
+        if ( -w $1/$i ) && ( -r $1/$i )
+          let cant++
+        fi
+      fi
+    done
+    echo "La cantidad de archivos con permiso de lectura y escritura es: $cant "
+    exit 0
+  else 
+    echo "No existe el directorio $1"
+    exit 4
+  fi
+fi
